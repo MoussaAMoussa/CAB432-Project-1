55d64244c00b613e47e4fc38e06094b1df52b32a
mossgit12 <mossmanpt456@gmail.com>
2025-09-24 14:37:22 +1000

    Spelling error in routes

--- processing.js
+++ processing.js
@@ -4,10 +4,10 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middlewares/uploadMiddleware");
-const processingController = require("../controller/processingController");
+const processingController = require("../controllers/processingcontroller");
 
 // POST /api/v1/process
 
-router.post("/process", upload.single("file"), processingController.processFile);
+router.post("/process", upload.single("file"), processingcontroller.processFile);
 
 module.exports = router;
